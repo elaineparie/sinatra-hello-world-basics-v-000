d6095e950950f31421568208045c0e824945be61
Elaine Anderson <eparie@bu.edu>
2018-07-13 11:24:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 
   # Define your GET '/' route below and respond with "Hello, World!"
   require 'sinatra'
-  require_relative 'config/environment'
+  #require_relative 'config/environment'
   class App < Sinatra::Base
 
     get '/' do
