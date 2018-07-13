514c351c0bfbbb10e6d27ceb2498e01b55e0fce1
Elaine Anderson <eparie@bu.edu>
2018-07-13 11:19:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  require 'sinatra'
+  class App < Sinatra::Base
 
+    get '/' do
+      "Hello, world!"
+    end
+
+  end
 
 end
